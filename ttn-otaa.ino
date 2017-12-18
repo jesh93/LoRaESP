05c3e2621cb665828b80243c59bda9bad59848a9
Unknown <johan.hinder@gmail.com>
2017-12-18 20:35:22 +0100

    Init code for Sodaq Explorer

---
+++ ttn-otaa.ino
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+ *
+ * Permission is hereby granted, free of charge, to anyone
+ * obtaining a copy of this document and accompanying files,
+ * to do whatever they want with them without any restriction,
+ * including, but not limited to, copying, modification and redistribution.
+ * NO WARRANTY OF ANY KIND IS PROVIDED.
+ *
+ * This example sends a valid LoRaWAN packet with payload "Hello,
+ * world!", using frequency and encryption settings matching those of
+ * the The Things Network.
+ *
+ * This uses OTAA (Over-the-air activation), where where a DevEUI and
+ * application key is configured, which are used in an over-the-air
+ * activation procedure where a DevAddr and session keys are
+ * assigned/generated for use with all further communication.
+ *
+ * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+ * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+ * violated by this sketch when left running for longer)!
+
+ * To use this sketch, first register your application and device with
+ * the things network, to set or generate an AppEUI, DevEUI and AppKey.
+ * Multiple devices can use the same AppEUI, but each device has its own
+ * DevEUI and AppKey.
+ *
+ * Do not forget to define the radio type correctly in config.h.
+ *
+ *******************************************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+//#define SerialUSB Serial
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8]={ 0xB5, 0x38, 0x00, 0xF0, 0x7E, 0xD5, 0xB3, 0x70 };
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8]={ 0x52, 0x39, 0x28, 0x61, 0x93, 0xA4, 0x8E, 0x00 };
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from ttnctl can be copied as-is.
+// The key shown here is the semtech default key.
+static const u1_t PROGMEM APPKEY[16] = { 0xE3, 0x5C, 0xB7, 0x30, 0x4B, 0xDE, 0xE9, 0xEF, 0x69, 0x94, 0x2A, 0xD3, 0xE4, 0xD9, 0xEA, 0x78 };
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+static uint8_t mydata[] = "Hello, world!";
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 20;
+
+// Pin mapping
+//Explorer
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 8,
+    .dio = {2, 3, LMIC_UNUSED_PIN},
+};
+//ESP
+// const lmic_pinmap lmic_pins = {
+//     .nss = 16,
+//     .rxtx = LMIC_UNUSED_PIN,
+//     .rst = 17,
+//     .dio = {20, 19, LMIC_UNUSED_PIN},
+// };
+
+void onEvent (ev_t ev) {
+    SerialUSB.print(os_getTime());
+    SerialUSB.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            SerialUSB.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            SerialUSB.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            SerialUSB.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            SerialUSB.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            SerialUSB.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            SerialUSB.println(F("EV_JOINED"));
+
+            // Disable link check validation (automatically enabled
+            // during join, but not supported by TTN at this time).
+            LMIC_setLinkCheckMode(0);
+            break;
+        case EV_RFU1:
+            SerialUSB.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            SerialUSB.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            SerialUSB.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            SerialUSB.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              SerialUSB.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              SerialUSB.println(F("Received "));
+              SerialUSB.println(LMIC.dataLen);
+              SerialUSB.println(F(" bytes of payload"));
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            SerialUSB.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            SerialUSB.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            SerialUSB.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            SerialUSB.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            SerialUSB.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            SerialUSB.println(F("Unknown event"));
+            break;
+    }
+}
+
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        SerialUSB.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        SerialUSB.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+    delay(2000);
+    SerialUSB.begin(115200);
+    SerialUSB.println(("Starting"));
+
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Start job (sending automatically starts OTAA too)
+    do_send(&sendjob);
+    SerialUSB.println(("Starting"));
+    
+}
+
+void loop() {
+  os_runloop_once();
+}
